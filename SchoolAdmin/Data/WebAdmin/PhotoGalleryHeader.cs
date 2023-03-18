0611aa9632f159253c254c14e5315c874f823fb9
Sourabh Raut <sourabh.raut@corecotechnologies.com>
2023-03-18 13:45:47 -0400

    Add project files.

---
+++ SchoolAdmin/Data/WebAdmin/PhotoGalleryHeader.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace SchoolAdmin.Data
+{
+    public class PhotoGalleryHeader
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter function name")]
+        public string? FunctionName { get; set; }
+        public string? FunctionDescription { get; set; }
+        public int SubscriberId { get; set; }
+        public bool IsStatus { get; set; } = true;
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+
+    }
+}
---
+++ SchoolAdmin/Data/WebAdmin/PhotoGalleyDetails.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolAdmin.Data
+{
+    public class PhotoGalleyDetails
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please select function")]
+        public string? FunctionId { get; set; }
+        [Required(ErrorMessage = "Please enter photo name")]
+        public string? ImageName { get; set; }
+        public int ImageOrder { get; set; }
+        public string? ImageDescription { get; set; }
+        public string? ImageUrl { get; set; }
+        public int SubscriberId { get; set; }
+        public bool Status { get; set; } = true;
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
