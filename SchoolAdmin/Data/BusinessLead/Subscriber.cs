e5e2f90d2ac37c9405982666c20a9e24b013f99e
Sourabh Raut <sourabh.raut@corecotechnologies.com>
2023-07-10 11:27:31 +0530

    Commit

---
+++ SchoolAdmin/Data/BusinessLead/Subscriber.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolAdmin.Data
+{
+    public class Subscriber
+    {
+        public Int64 SubscriberMasterId { get; set; }
+        public string SubscroberCode { get; set; }
+        public string SubscriberName { get; set; }
+        public string SubscriberAddress { get; set; }
+        public string Email{ get; set; }
+        public string Mobile { get; set; }
+    }
+}
--- SchoolAdmin/Program.cs
+++ SchoolAdmin/Program.cs
@@ -3,12 +3,23 @@ using Microsoft.AspNetCore.Components.Web;
 using SchoolAdmin.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var baseAddress = builder.Configuration.GetValue<string>("BaseUrl");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<HttpClient>();
+
+HttpClientHandler clientHandler = new HttpClientHandler();
+clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+builder.Services.AddSingleton(new HttpClient(clientHandler)
+{
+    BaseAddress = new Uri(baseAddress)
+});
+
+
 
 var app = builder.Build();
 
@@ -17,10 +28,12 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // app.UseHsts();
+    
+
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
